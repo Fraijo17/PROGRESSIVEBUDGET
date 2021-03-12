c7c566e6e24cda3004d7288e4edad30a335dc3ce
Fraijo17 <abrahamfraijo17@gmail.com>
2021-03-12 08:49:13 -0700

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/tracker2", {
   useNewUrlParser: true,
   useUunifiedTopology: true,
   useCreateIndex: true,
